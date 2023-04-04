e94bbf332169586b68d9458945ed671bc400848e
rumentsolov <r.tsolov86@gmail.com>
2023-04-04 19:38:55 +0300

    server added

--- js/server.js
+++ js/server.js
@@ -1,10 +1,19 @@
 //alert("Hello JS!");
 
-const express = require('express'); 
+const express = require("express"); 
 
 const app = express();
 
+const https = require("http"); // SSL is still unknown for me
+
 app.get("/",function(req,res){
+
+    const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f6b0cb5b903bf06fe77b47d1818b6ad2";
+
+    https.get(weatherUrl, function(response){
+        console.log(response);
+    })
+
     var path = require("path");
     res.sendFile(path.normalize(__dirname + "/../html/index.html"));
 })
